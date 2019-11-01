f3248e16486c2c79f243621efe4a871e2788f1d8
XOKAGE95 <56272442+XOKAGE95@users.noreply.github.com>
2019-11-01 21:41:35 +0400

    Теперь все работает

--- laba3/laba3.cpp
+++ laba3/laba3.cpp
@@ -10,7 +10,7 @@
 #define m_pi 3.141592
 using namespace std;
 
-
+float eps = 0.000001;
 /*
 Лабораторная 3 будет весь раздел "СВОБОДНОЕ ПАДЕНИЕ ТЕЛА С УЧЕТОМ СОПРОТИВЛЕНИЯ СРЕДЫ".
 Везде надо будет строить графики h(t), v(t), a(t) согласно своему варианту, который соответствует номеру записи в журнале старосты
@@ -18,9 +18,8 @@ using namespace std;
 
 Case1 (float v, float dt)
 {
-	float m = 65, h = 1.5 , r = 0.5 , c = 1.22, k2, dh = 0, a = 0, vi, t = 0;
+	float m = 65, h = 1.5 , r = 0.5 , c = 1.22, k2, dh = 0, a = 0, ai = 0, vi, t = 0;
     vi = v;
-    float eps = 0.00001;
     k2 = h * r * 0.5 * c * 1.29;
     ofstream file;
     file.open("case 1.txt");
@@ -28,9 +27,10 @@ Case1 (float v, float dt)
     do
     {
         v = vi;
+        a = ai;
 
         a = (m * g - k2 * v * v)/m;
-        file << setw(15) << left << a;
+        file << setw(15) << setprecision(5) << left << a;
 
 
         vi = v + dt/2*((m*g - k2*v*v)/m + (m*g - k2*pow((v + dt * (m*g - k2*v*v)/m), 2))/m);
@@ -51,7 +51,6 @@ Case2 (float v, float dt)
 {
     float m = 80, r = 2.5 , c = 0.4, k2, dh = 0, a = 0, vi, t = 0;
     vi = v;
-    float eps = 0.00001;
     k2 = m_pi * r * r * 0.5 * c * 1.29;
     ofstream file;
     file.open("case 2.txt");
@@ -61,7 +60,7 @@ Case2 (float v, float dt)
         v = vi;
 
         a = (m * g - k2 * v * v)/m;
-        file << setw(15) << left << a;
+        file << setw(15) << setprecision(5) << left << a;
 
 
         vi = v + dt/2*((m*g - k2*v*v)/m + (m*g - k2*pow((v + dt * (m*g - k2*v*v)/m), 2))/m);
@@ -82,9 +81,9 @@ Case3 (float v, float dt)
 {
     float m, r = 0.1 , ro_b = 8930, ro_s = 1000, k1, dh = 0, a = 0, vi, t = 0, mu = 1180;
     vi = v;
-    float eps = 0.00001;
     float V = 4/3*m_pi*r;
     k1 = 6*m_pi*r*mu;
+    m = ro_b*V;
     float m1 = (ro_b - ro_s) * V;
     ofstream file;
     file.open("case 3.txt");
@@ -94,7 +93,7 @@ Case3 (float v, float dt)
         v = vi;
 
         a = (m * g - k1 * v)/m;
-        file << setw(15) << left << a;
+        file << setw(15) << setprecision(5) << left << a;
 
 
         vi = v + dt/2*((m1*g - k1*v)/m + (m1*g - k1*((v + dt * (m1*g - k1*v)/m)))/m);
