07fb6dd15bbad822c16cbe8b7ce988722c27eb59
XOKAGE95 <56272442+XOKAGE95@users.noreply.github.com>
2019-11-15 20:21:57 +0300

    Сделал пару разных штук

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -12,17 +12,17 @@ const double G = 6.67408E-11;
 const double M = 5.97E24;
 const double R = 6.37E6;
 const double Betta = 1.29E-4;
-const double Alfa = 3135;
+const double Alfa = 2520;
 const double c = 0.45;
 const double S = 83.322;
-const double F = 4 * 1E6;
-
+const double F = 3E6;
+//const double I = 2520;
 using namespace std;
 
 double dt;
 int COUNT = 0;
 
-void Display(double a, double v, double h, double t)
+void Display1(double a, double v, double h, double t)
 {
     if (COUNT == 0)
     {
@@ -30,14 +30,28 @@ void Display(double a, double v, double h, double t)
     file.open("Case_1.txt", ios_base::app);
     file << setw(15) << setprecision(5) << left << a;
     file << setw(15) << setprecision(10) << left << v;
-    file << setw(15) << setprecision(4) << left << h;
-    file << setw(15) << setprecision(3) << left << t << endl;
+    file << setw(15) << setprecision(7) << left << h;
+    file << setw(15) << setprecision(7) << left << t << endl;
+    file.close();
+    }
+}
+
+void Display2(double a, double v, double h, double t)
+{
+    if (COUNT == 0)
+    {
+    ofstream file;
+    file.open("Case_2.txt", ios_base::app);
+    file << setw(15) << setprecision(5) << left << a;
+    file << setw(15) << setprecision(10) << left << v;
+    file << setw(15) << setprecision(7) << left << h;
+    file << setw(15) << setprecision(7) << left << t << endl;
     file.close();
     }
 }
 void render_scene()
 {
-double m, a, g, ro, vi, ai;
+double m, a, g, ro, vi, ai, v2;
                     // Зададим начальные данные
 double h = 0;
 double t = 0;
@@ -48,15 +62,17 @@ double v = 0;
 double k2 = c*S*ro;
                     // Формула для коэффициента сопротивления среды
 
-double V = 2096;
+double V = 1692.762027;
+double A = 90.359;
+double H = 38618.14;
 double T = (m0 - mk) / Alfa;
 
-                // Оси
-    glClearColor(1, 1, 1, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glLineWidth(1.0);
-    glColor3d(0, 0, 0);
-    glBegin(GL_LINES);
+                // Прорисовка координатной оси
+    glClearColor(1, 1, 1, 1.0);     // Цвет фона - белый
+    glClear(GL_COLOR_BUFFER_BIT); //Очистка экрана (На случай, если там был трэш)
+    glLineWidth(1.0);       //Толщина линий
+    glColor3d(0, 0, 0);     //Цвет линий - черный
+    glBegin(GL_LINES);  //Прорисовка линиями (рисуются линии для каждых 2х точек)
     glVertex3d(-1.4, 0.0, -5.0);
     glVertex3d(1.4, 0.0, -5.0);
     glEnd();
@@ -76,16 +92,40 @@ double T = (m0 - mk) / Alfa;
     glVertex3d(-0.1, 1.2, -5.0);
     glVertex3d(0.0, 1.4, -5.0);
     glEnd();
+
+                        //Прорисовка букв на координатных осях
+
+    glColor3d(0.0, 0.0, 0.0);
     glRasterPos3d(1.4, 0.1, -5.0);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 'V');
-    glRasterPos3d(0.1, 1.4, -5.0);
     glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 'T');
+
+    glColor3d(1.0, 0.0, 0.5);
+    glRasterPos3d(0.1, 1.4, -5.0);
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 'V');
+
+    glColor3d(0.0, 0.0, 0.0);
+    glRasterPos3d(0.17, 1.4, -5.0);
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ',');
+
+    glColor3d(0.0, 1.0, 0.5);
+    glRasterPos3d(0.2, 1.4, -5.0);
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 'A');
+
+    glColor3d(0.0, 0.0, 0.0);
+    glRasterPos3d(0.27, 1.4, -5.0);
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ',');
+
+    glColor3d(0.5, 0.0, 1.0);
+    glRasterPos3d(0.30, 1.4, -5.0);
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 'H');
+
 
             //Граффик
     glBegin(GL_POINTS);
 
     v = 0;
     vi = 0;
+    v2 = 0;
     do
     {
         m = (m0- Alfa * t);		// Формула для массы ракеты
@@ -93,23 +133,31 @@ double T = (m0 - mk) / Alfa;
         ro = ro0* exp(-Betta*h); 	// Формула для плотности среды
 
 
+
+        ai = (vi - v2)/dt;
+        v2 = vi;
+        vi = v2 + dt/2*((F - m*g - k2*v2*v2)/m + (F - m*g - k2*pow((v2 + dt * (F - m*g - k2*v2*v2)/m), 2))/m);
+        Display2(ai, vi, h, t); //*/
+
         a = (F - m*g - k2 * v * v)/m;
         v += a * dt; //*/
 
-
-        /*ai = (vi - v)/dt;
-        v = vi;
-        vi = v + dt/2*((m*g - k2*v*v)/m + (m*g - k2*pow((v + dt * (m*g - k2*v*v)/m), 2))/m);//*/
-
-        h += v * dt;
+        h += v2 * dt;
         t += dt;
 
         glColor3d(1.0, 0.0, 0.5);
         glLineWidth(2.0);
         glVertex3d(t/T, v/V, -5.0);//*/
 
-        //Display(ai, vi, h, t);
-        Display(a, v, h, t);
+        glColor3d(0.0, 1.0, 0.5);
+        glLineWidth(2.0);
+        glVertex3d(t/T, a/A, -5.0);//*/
+
+        glColor3d(0.5, 0.0, 1.0);
+        glLineWidth(2.0);
+        glVertex3d(t/T, h/H, -5.0);//*/
+
+        Display1(a, v, h, t);
 
     }
     while (m > mk);
@@ -117,7 +165,10 @@ double T = (m0 - mk) / Alfa;
     if (COUNT == 0)
     {
     COUNT ++;
-    cout << "Fact Time = " << t << " Seconds" << endl;
+    cout << "Factual Time = " << t << " Seconds" << endl;
+   /* cout << "Theoretical Time = " << T << " Seconds" << endl;
+    cout << "Factual speed = " << v << " m/s "<< endl;
+    cout << "Theoretical speed = " << V << " m/s "<< endl;*/
     }
     glutSwapBuffers();
 
@@ -131,7 +182,7 @@ void changeSize(int w, int h)
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glViewport(0, 0, w, h);
-	gluPerspective(40,ratio,3,1000);
+	gluPerspective(35,ratio,3,1000);
 	gluOrtho2D(0, 0, 0, 0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -144,12 +195,20 @@ int main(int argc, char **argv)
     cout << "Enter dt" <<endl;
     cin >> dt;
     ofstream file;
-    file.open("case_1.txt");
+    file.open("Case_1.txt");
     file << setw(15) << left << "Acceleration";
     file << setw(15) << left << "Speed";
     file <<  setw(15) << left << "Height";
     file << setw(15) << left << "Time" << endl;
     file.close();
+
+    ofstream file1;
+    file1.open("Case_2.txt");
+    file1 << setw(15) << left << "Acceleration";
+    file1 << setw(15) << left << "Speed";
+    file1 <<  setw(15) << left << "Height";
+    file1 << setw(15) << left << "Time" << endl;
+    file1.close();
     glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(860, 620);
